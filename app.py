3ca5fba3174724febc5a1a0bdf6c109c86b6a377
RS2891 <58849989+RS2891@users.noreply.github.com>
2021-04-28 19:42:19 +0530

    Hi

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-import numpy as np
-from flask import Flask, request, jsonify
-import pickle
-app = Flask(__name__)
-# Load the model
-model = pickle.load(open('model.pkl','rb'))
+import numpy as np
+from flask import Flask,request,jsonify
+import pickle
+app=Flask(__name__)
+model=pickle.load(open('model.pkl','rb'))
+#@app.route('/api',methods=['POST'])
 @app.route('/api',methods=['POST'])
-def predict():
-    # Get the data from the POST request.
-    data = request.get_json(force=True)
-    # Make prediction using model loaded from disk as per the data.
-    prediction = model.predict([[np.array(data['exp'])]])
-    # Take the first value of prediction
-    output = prediction[0]
-    return jsonify(output)
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+def predict():
+  data=request.get_json(force=True)
+  predictions=model.predict([[np.array(data['exp'])]])
+  output=prediction[0]
+  return jsonify(output)
